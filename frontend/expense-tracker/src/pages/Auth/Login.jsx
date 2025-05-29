bac876e7dcde940c4b4e6427167b477246226baf
anukriti_23 <anukriti2306@gmail.com>
2025-05-29 15:33:05 +0530

    git commit -m 'pushing buggy code but what choice do i have?'

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,7 +1,8 @@
 import React , {useState} from 'react';
 import AuthLayout from '../../components/layouts/AuthLayout';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
+import { validateEmail } from "../../utils/helper";
 const Login = () =>{
   const [email, setEmail] = useState("");
   const [password, setPassword]=useState("");
@@ -25,6 +26,24 @@ const Login = () =>{
             placeholder="john@example.com"
             type="text"
         />
+
+        <Input 
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 Characters"
+            type="password"
+        />
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+        <button type="submit" className="btn-primary">
+        LOGIN
+        </button>
+        <p className="text-[13px] text-slate-800 mt-3">
+            Don't have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/signup">
+              SignUp
+            </Link>
+        </p>
         </form>
       </div>
     </AuthLayout>
