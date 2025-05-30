27eeecaded60aeb09e930649a3d9774943da6602
anukriti_23 <anukriti2306@gmail.com>
2025-05-30 15:14:52 +0530

    git commit -m 'settings files'

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -11,7 +11,7 @@ import SignUp from "./pages/Auth/SignUp";
 import Home from "./pages/Dashboard/Home";
 import Income from "./pages/Dashboard/Income";
 import Expense from "./pages/Dashboard/Expense";
-
+import './index.css';
 const App = () =>{
   return(
     <div>
