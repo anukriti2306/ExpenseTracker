bc2f658754d37ad78d1603f4611f435609953c6b
anukriti_23 <anukriti2306@gmail.com>
2025-05-30 14:34:03 +0530

    coded the profile photo selector and the sign up UI

--- frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
+++ frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CARD_2 from '../../assets/images/card2.png';
 import { LuTrendingUpDown } from "react-icons/lu";
+
 const AuthLayout = ({children}) =>{
   return <div className="flex">
         <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
