3ac11a54694f75f2f55cdc54afda0e30438697f2
anukriti_23 <anukriti2306@gmail.com>
2025-06-01 21:33:23 +0530

    git commit -m 'finished up the get uesr info and half income apis'

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,4 +1,5 @@
 const express=require("express");
+const { protect } = require("../middleware/authMiddleware");
 const{
     registerUser,
     loginUser,
@@ -8,6 +9,6 @@ const router=express.Router();
 
 router.post("/register",registerUser);
 router.post("/login",loginUser);
-//router.get("/getUser", protect, getUserInfo);
+router.get("/getUser", protect, getUserInfo);
 
 module.exports=router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors=require("cors");
 const path=require("path");
 const connectDB=require("./config/db");
 const authRoutes=require("./routes/authRoutes");
+const incomeRoutes=require("./routes/incomeRoutes");
 
 const app=express();
 
@@ -20,5 +21,6 @@ app.use(
 app.use(express.json());
 connectDB();
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server listening on PORT ${PORT}`));
