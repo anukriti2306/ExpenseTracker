b70f3642596af76eab840da168eca9a9f700f609
anukriti_23 <anukriti2306@gmail.com>
2025-06-07 11:31:35 +0530

    added the recent transactions button, styling

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -9,6 +9,7 @@ import InfoCard from "../../components/Cards/InfoCard";
 import {addThousandsSeparator} from "../../utils/helper";
 import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
 import {IoMdCard} from "react-icons/io";
+import RecentTransactions from "../../components/Dashboard/RecentTransactions";
 const Home = () =>{
   useUserAuth();
   const navigate = useNavigate();
@@ -63,6 +64,13 @@ const Home = () =>{
             color="bg-red-500"
           /> 
                 
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+        <RecentTransactions
+          transactions={dashboardData?.recentTransactions}
+          onSeeMore={()=>navigate("/expense")}
+        />
+
       </div>
       </div>
     </DashboardLayout>
