2287615a0d188d760c9fe01747f5e943a9ff4d71
anukriti_23 <anukriti2306@gmail.com>
2025-06-09 21:19:29 +0530

    git commit -m 'changed the frontend a little bit'

--- frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
+++ frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
@@ -4,8 +4,8 @@ import { LuTrendingUpDown } from "react-icons/lu";
 
 const AuthLayout = ({children}) =>{
   return <div className="flex">
-        <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-            <h2 className="text-lg font-bold text-black">Expense Tracker</h2>
+        <div className="w-screen h-screen bg-pink-200 md:w-[60vw] px-12 pt-8 pb-12">
+            <h2 className="text-3xl justify-between font-serif font-bold text-black">Expense Tracker</h2>
             {children}
         </div>
         <div className="hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
