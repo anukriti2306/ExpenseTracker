3a3eda3a1b15fef1aba4d126fe43c03fb6f58a01
anukriti_23 <anukriti2306@gmail.com>
2025-06-11 18:59:31 +0530

    pushing changes to deploy the frontend and backend

--- frontend/expense-tracker/src/utils/axiosInstance.js
+++ frontend/expense-tracker/src/utils/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { BASE_URL } from "./apiPaths";
+//import { BASE_URL } from "./apiPaths";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
 const axiosInstance = axios.create({
     baseURL : BASE_URL,
