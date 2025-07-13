50a8be9c427e2bf9a55c47f35a91c9f33677833c
anukriti_23 <anukriti2306@gmail.com>
2025-07-13 12:10:03 +0530

    fixing the cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 connectDB();
 
 const allowedOrigins = [
-  "https://expense-tracker-weld-psi-21.vercel.app",
+  "https://expense-tracker-weld-psi-21.vercel.app/login",
   "http://localhost:5173" // (optional, for local testing)
 ];
 
